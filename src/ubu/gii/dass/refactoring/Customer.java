e048af4b890587c8198e8e3c434ab2a3a98b3e98
AhmadMarPas <AhmadMarPas@users.noreply.github.com>
2025-05-02 16:02:59 +0200

    (#1) Realizada la tarea de refactorización que incluye las siguientes acciones: - Extract Method - Rename - Move Method - Change Method Signature Se incluye historial del Refactoring.

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -3,14 +3,21 @@ package ubu.gii.dass.refactoring;
 /**
 * Tema  Refactorizaciones 
 *
-* Ejemplo de aplicaci�n de refactorizaciones. Actualizado para colecciones gen�ricas de java 1.5
+* Ejemplo de aplicación de refactorizaciones. Actualizado para colecciones genéricas de java 1.5
 *
-* @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos L�pez</A>
-* @version 1.1
+* @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos López</A>
+* @author <a HREF="mailto:azs1004@alu.ubu.es">Adrián Zamora Sánchez</a>
+* @author <a HREF="mailto:edr1006@alu.ubu.es">Estíbalitz Diéz Rioja</a>
+* @author <a HREF="mailto:ldg1008@alu.ubu.es">Luis Ignacio De Luna Gómez</a>
+* @author <a HREF="mailto:amp1048@alu.ubu.es">Ahmad Mareie Pascual</a>
+* @since 1.1
+* @version 1.2
 * @see java.io.File
 *
 */
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Customer {
 	private String _name;
@@ -37,39 +44,19 @@ public class Customer {
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasNext()) {
 			double thisAmount = 0;
-			Rental each = rentals.next();
+			Rental rental = rentals.next();
 			// determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-			}
+			thisAmount = rental.calculateAmount();
 			
 			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
-				frequentRenterPoints++;
+			frequentRenterPoints += rental.calculateFrequentRenterPoints();
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
+			result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
 			totalAmount += thisAmount;
 		}
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
-				+ " frequent renter points";
+		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 		return result;
 	}
 }
--- src/ubu/gii/dass/refactoring/Rental.java
+++ src/ubu/gii/dass/refactoring/Rental.java
@@ -6,7 +6,12 @@ package ubu.gii.dass.refactoring;
  * genéricas de java 1.5.
  * 
  * @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos López</A>
- * @version 1.1
+ * @author <a HREF="mailto:azs1004@alu.ubu.es">Adrián Zamora Sánchez</a>
+ * @author <a HREF="mailto:edr1006@alu.ubu.es">Estíbalitz Diéz Rioja</a>
+ * @author <a HREF="mailto:ldg1008@alu.ubu.es">Luis Ignacio De Luna Gómez</a>
+ * @author <a HREF="mailto:amp1048@alu.ubu.es">Ahmad Mareie Pascual</a>
+ * @since 1.1
+ * @version 1.2
  * @see java.io.File
  * 
  */
@@ -25,6 +30,44 @@ public class Rental {
 
 	public Movie getMovie() {
 		return _movie;
+	}
+
+	/**
+	 * Calcula el importe de una película en función de su tipo y los días.
+	 * 
+	 * @return
+	 */
+	public double calculateAmount() {
+		double amount = 0;
+		switch (getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			amount += 2;
+			if (getDaysRented() > 2)
+				amount += (getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			amount += getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			amount += 1.5;
+			if (getDaysRented() > 3)
+				amount += (getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return amount;
+	}
+
+	/**
+	 * Calcula los puntos de alquiler frecuentes. Se le añade un punto por cada.
+	 * 
+	 * @return
+	 */
+	public int calculateFrequentRenterPoints() {
+		int frequentRenterPoints = 1;
+		// add bonus for a two day new release rental
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+			frequentRenterPoints++;
+		return frequentRenterPoints;
 	}
 
 }
